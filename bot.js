ae240f0c822dac6f3d416003097ddc7d231c7d0e
EQWTQWSd <144906432+EQWTQWSd@users.noreply.github.com>
2024-03-31 18:52:05 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,8 +10,8 @@
 
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KyxUC4h8K6 ]
+   ## YT : https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw
 */
 
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
@@ -139,6 +139,6 @@ printWatermark();
 
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KyxUC4h8K6 ]
+   ## YT : https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw
 */
