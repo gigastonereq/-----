af5e038c7f223136614d78fbb53abfcf6c33bc56
EQWTQWSd <144906432+EQWTQWSd@users.noreply.github.com>
2024-03-31 19:05:21 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -10,8 +10,8 @@
    
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KyxUC4h8K6 ]
+   ## YT : https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw
 */
 const { ApplicationCommandOptionType } = require('discord.js');
 const db = require("../mongoDB");
@@ -24,8 +24,8 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const youtubeLink = 'https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw';
+      const InstagramLink = '';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
@@ -34,7 +34,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ ขอบคุณที่เข้ามาใช้งาย BOT ของเราผมมีชื่อว่า NO_NAME เป็นเจ้าของ bot ตัวนี้ขอบคุณเป็นอย่างสูงที่เข้ามาใช้ : ❤️ [NO_NAME](${youtubeLink})\n Instagram : 💙 [none](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
@@ -55,6 +55,6 @@ module.exports = {
    
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KyxUC4h8K6 ]
+   ## YT : https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw
 */
