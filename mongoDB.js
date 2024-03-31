2d02c44701c72f7dff896f2f871e34cd9931a779
EQWTQWSd <144906432+EQWTQWSd@users.noreply.github.com>
2024-03-31 18:53:16 +0700

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -10,8 +10,8 @@
    
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KyxUC4h8K6 ]
+   ## YT : https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw
 */
 const { Schema, model } = require("mongoose");
 
@@ -46,6 +46,6 @@ module.exports = {
    
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KyxUC4h8K6 ]
+   ## YT : https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw
 */
