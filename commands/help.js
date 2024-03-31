91ade4539b6ab434239eb3fb327724bd33be6d1c
EQWTQWSd <144906432+EQWTQWSd@users.noreply.github.com>
2024-04-01 03:10:46 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -57,7 +57,7 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('https://i.pinimg.com/originals/f0/68/95/f06895b19b937c8318840d04b7f1fa04.gif')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
         .setURL('https://www.youtube.com/channel/UCFeiE-VP3sEuBeKhJnHdoYw')
